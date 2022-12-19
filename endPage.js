63ec217725f2b1036c4a858ed576cc724ab33265
comicsannotationtool <le300.research@gmail.com>
2022-12-19 17:41:03 +0000

    get story 1 up to finish the story 1...

--- endPage.js
+++ endPage.js
@@ -200,7 +200,7 @@ function submitVFLI(event) {
         //                          jsonString: jsonString
         //                          });
         
-        db.collection("Char_Experiment_4").add({time: Date().toLocaleString(),
+        db.collection("Char_Experiment_1_2").add({time: Date().toLocaleString(),
                                                     jsonData: jsonString});
         
         console.log("Submit to Firebase");
